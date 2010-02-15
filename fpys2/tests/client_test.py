5585a7df16ac0ce8952325cb5bf41f3a0b36b5ec
Wade Simmons <wsimmons@gmail.com>
2010-02-15 14:01:55 -0700

    remove tests for methods that don't exist anymore

--- fpys2/tests/client_test.py
+++ fpys2/tests/client_test.py
@@ -32,55 +32,6 @@ def test_get_signature():
     signature = "XhQ1h0ro5eUU2fCZSz4rNNbYDrwHNi4dpfaTe9VrZTE="
     assert fps_client.get_signature(parameters, "/capture") == signature, "Signatures do not match"
 
-def test_getAccountBalance():
-    response = fps_client.getAccountBalance()
-    assert 16.5 == response.accountBalance.totalBalance.amount
-
-# def test_getDebtBalance():
-#     response = fps_client.getDebtBalance(instrument_id = "123")
-#     assert 0.0 == "blah"
-
-def test_installPaymentInstruction():
-    """
-    Install a valid instruction
-    """
-
-    unique = "4685bc1eef1311dc952e00142241a3a2"
-
-    # This call should work
-    response = fps_client.installPaymentInstruction(payment_instruction="MyRole == 'Caller';",
-                                                    caller_reference="fpes.achievewith.us_caller" + unique,
-                                                    token_type="Unrestricted",
-                                                    token_friendly_name="fpes.achievewith.us_caller" + unique)
-    assert response.success == True
-    assert response.tokenId == "ZS4X8G44GEIVGVSEN2DI5NDO6Q2WX3JQ9125FNR8IBLF5CFH8ZMT3RLNBJUJH9MN"
-
-
-def test_installPaymentInstructionDuplicate():
-    """Attempt to install a duplicate instruction"""
-    unique = "4685bc1eef1311dc952e00142241a3a2"
-    # This call is a duplicate of the last one, it fails
-    response = fps_client.installPaymentInstruction(payment_instruction="MyRole == 'Caller';",
-                                                    caller_reference="fpes.achievewith.us_caller" + unique,
-                                                    token_type="Unrestricted",
-                                                    token_friendly_name="fpes.achievewith.us_caller" + unique)
-    assert response.success == False
-    assert len(response.errors) == 1
-    assert response.errors[0].errorCode == "DuplicateRequest"
-
-def test_installPaymentInstructionInvalid():
-    """Attempt to install an invalid instruction"""
-    # This call has an invalid instruction
-    unique = "1234567890"
-    response = fps_client.installPaymentInstruction(payment_instruction="Invalid Instruction;",
-                                                    caller_reference="fpes.achievewith.us_caller" + unique,
-                                                    token_type="Unrestricted",
-                                                    token_friendly_name="fpes.achievewith.us_caller" + unique)
-    assert response.success == False
-    assert len(response.errors) == 2
-    assert response.errors[0].reasonText.startswith("Parse errors")
-
-
 def test_cancelToken():
     """Cancel a valid token"""
     token_id = "Z24XPGA4G3IMGV1EL2DL5KDOKQ4WXZJL9175MNR5I5LF1CKH8UMK3R5NFJUEHXMQ"
@@ -93,30 +44,6 @@ def test_cancelTokenInvalid():
     response = fps_client.cancelToken(token_id)
     assert response.success == False
     assert response.errors[0].errorCode == "InvalidParams"
-
-def test_discardResults():
-    """Test DiscardResults"""
-    response = fps_client.discardResults("135AHMQA9H3NEFJL73GQ33873PLPNGLQZP1")
-    assert response.success == True
-
-def test_getAccountActivity():
-    response = fps_client.getAccountActivity("2008-04-13")
-    assert response.success == True
-    assert response.responseBatchSize == 8
-    trans = response.transactions[0]
-    assert trans.transactionParts.feePaid.amount == 0.0
-    assert trans.callerTokenId == "Z34XMGF4GCILGV7EV2D45DDO4Q6WXEJZ9175UNR5I9LFEC1H8MMX3R6NBJUJH8MQ"
-    assert trans.operation == "Pay"
-    assert trans.dateReceived.month == 4
-
-def test_getPaymentInstruction():
-    """Retrive an existing payment instruciton"""
-    response = fps_client.getPaymentInstruction("ZS4X8G44GEIVGVSEN2DI5NDO6Q2WX3JQ9125FNR8IBLF5CFH8ZMT3RLNBJUJH9MN")
-    assert response.success == True
-    assert response.token is not None
-    assert response.token.status == 'Active'
-    assert response.token.tokenType == 'Unrestricted'
-    assert response.paymentInstruction.startswith("MyRole =")
 
 def test_getTokenByCaller():
     response = fps_client.getTokenByCaller(token_id='Z34XMGF4GCILGV7EV2D45DDO4Q6WXEJZ9175UNR5I9LFEC1H8MMX3R6NBJUJH8MQ')
@@ -136,31 +63,6 @@ def test_getTokenByCaller():
     assert response.token.dateInstalled.year == 2008
     assert response.token.dateInstalled.month == 3
     assert response.token.dateInstalled.day == 10
-
-def test_getTokenUsageInvalid():
-    """Retrieve token usage for a SingleUse token"""
-    # GetTokenUsage is only valid for multi-use tokens
-    response = fps_client.getTokenUsage("Z74XLGQ4GSIKGV2ES2DQ5GDOCQZWXIJV9195JNRZIVLFSC1H84M33RDN3JUGHFM5")
-    assert response.success == False
-    assert response.errors[0].errorCode == 'InvalidTokenType'
-
-def test_getTokenUsageUnrestricted():
-    """Retrieve token usage for an unrestricted token"""
-    response = fps_client.getTokenUsage("Z54XNG14GBILGV8EM2D95FDOZQHWX3JT91X5CNR8I3LFICUH88MU3RBNZJUNHGM7")
-    assert response.success == True
-
-def test_getTokenUsage():
-    """Retrieve a token with usage restrictions"""
-    # TODO sample, please?
-    pass
-
-def test_getTransaction():
-    response = fps_client.getTransaction("135AHMQA9H3NEFJL73GQ33873PLPNGLQZP1")
-    assert response.success == True
-    assert response.transaction.transactionId == "135AHMQA9H3NEFJL73GQ33873PLPNGLQZP1"
-    assert response.transaction.senderName == "Kate M DeHaven"
-    assert response.transaction.operation == "Pay"
-    assert response.transaction.paymentMethod == "CC"
 
 def test_pay():
     """Initiates a payment"""
@@ -197,11 +99,6 @@ def test_reserve():
     assert response.success == True
     assert response.transaction.status == "Initiated"
     assert response.transaction.transactionId == "134OLF7MHB2L4V9T54RHADQ9FCK5NLVZHDC"
-
-def test_retry():
-    response = fps_client.retryTransaction("123")
-    assert response.success == True
-    assert response.transaction.status == "Initiated"
 
 def test_settle_over_amount():
     response = fps_client.settle("134OLF7MHB2L4V9T54RHADQ9FCK5NLVZHDC",
